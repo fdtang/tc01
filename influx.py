4c28bde16dc322aeb385a1ddb63e59a6c750c306
Francois Tang <francois.tang@hiptec.no>
2023-11-16 10:22:53 +0100

    update influxdb

--- influx.py
+++ influx.py
@@ -1,41 +1,38 @@
 from influxdb_client import InfluxDBClient, Point
 from influxdb_client.client.write_api import SYNCHRONOUS
 
-class InfluxDB() : 
-  '''
-      Author: F. Tang
-      Last Revision: August 17th, 2021
-      Description: Send sound level
-  '''
-    
-  def __init__(self) :
-    
-    influxClient = InfluxDBClient(url="http://winston.hiptec.no:8086", 
-                                       token="4ScEnBn7HAd-bTb6-Z9lOX7tB_aN04kQiFh59LOLk8ASCBAoC0hjcsd6m4vRSHcOQMZoyh6MuDmlII4zHsUysA==",
-                                       org="HIPtec")
-    
-    self.write_api = influxClient.write_api(write_options=SYNCHRONOUS)
-
-  def send(self, temperature_body) : 
+class InfluxDB() :
     '''
-      Author: F. Tang
-      Last Revision: July 7th, 2020
-      Description: Send sound level data over to InfluxDB
-    '''  
-    p = Point("temperature").tag("box_id", "tc01")
-              
-    
-    for temp_index in range(0, 4): 
-      try:
-        temperature = float(temperature_body[temp_index])
-        if temperature> -9998 :
-          p = p.field(f"ch{temp_index}" , temperature)
-      except:
-        pass
-          
+        Author: F. Tang
+        Last Revision: November 23th, 2023
+        Description: Send sound level
+    '''
+    def __init__(self) :
 
-    try:  
-      if temperature_body:      
-        self.write_api.write(bucket="production", record=p)
-    except : 
-      pass
+        influxClient = InfluxDBClient(url="http://192.168.1.241",
+                                      token="bdnqhThdNULGU8tXcmobINXlNVpzDfEc0UWwpydDlxL_hB_q5-KJEqKhkeBupg4jZZm96CeyjMQFDC2swJNw3A==",
+                                      org="HIPtec")
+
+        self.write_api = influxClient.write_api(write_options=SYNCHRONOUS)
+
+    def send(self, temperature_body) :
+        '''
+            Author: F. Tang
+            Last Revision: July 7th, 2020
+            Description: Send sound level data over to InfluxDB
+        '''
+        p = Point("temperature").tag("box_id", "tc01")
+
+        for temp_index in range(0, 4):
+            try:
+                temperature = float(temperature_body[temp_index])
+                if temperature> -9998 :
+                    p = p.field(f"ch{temp_index}" , temperature)
+            except:
+                pass
+
+        try:
+            if temperature_body:
+                self.write_api.write(bucket="production", record=p)
+        except :
+            pass
--- logger.py
+++ logger.py
@@ -11,7 +11,7 @@
  	Channel 4 : Open k-type thermocouple
 '''
 import time
-import random 
+import random
 from influx import InfluxDB
 
 
@@ -20,29 +20,28 @@ from daqhats_utils import select_hat_device, tc_type_to_string
 
 if __name__ == '__main__':
 
-  ## You can generate a Token from the "Tokens Tab" in the UI  
-  starttime=time.time()
-  influx = InfluxDB()
-  try:
-    address = select_hat_device(HatIDs.MCC_134)
-    hat = mcc134(address)
-    tc_type = TcTypes.TYPE_K   # change this to the desired thermocouple type
-    delay_between_reads = 1  # Seconds
-    channels = (0, 1, 2, 3)
-    
-          
-    for channel in channels:
-        hat.tc_type_write(channel, tc_type)
-         
-    while True:
-      temperatures = {0: None, 1:None, 2: None, 3: None}
-      for channel in channels:
-        value = hat.t_in_read(channel)
-        if value not in [mcc134.OPEN_TC_VALUE, mcc134.OVERRANGE_TC_VALUE,  mcc134.COMMON_MODE_TC_VALUE] : 
-          temperatures[channel] = value
+    ## You can generate a Token from the "Tokens Tab" in the UI
+    starttime=time.time()
+    influx = InfluxDB()
+    try:
+        address = select_hat_device(HatIDs.MCC_134)
+        hat = mcc134(address)
+        tc_type = TcTypes.TYPE_K   # change this to the desired thermocouple type
+        delay_between_reads = 1  # Seconds
+        channels = (0, 1, 2, 3)
 
-      influx.send(temperatures)
-      time.sleep(1 - (time.time() - starttime) % 1)
-      
-  except (HatError, ValueError) as error:
-    print('\n', error)
+        for channel in channels:
+            hat.tc_type_write(channel, tc_type)
+
+        while True:
+            temperatures = {0: None, 1:None, 2: None, 3: None}
+            for channel in channels:
+                value = hat.t_in_read(channel)
+                if value not in [mcc134.OPEN_TC_VALUE, mcc134.OVERRANGE_TC_VALUE,  mcc134.COMMON_MODE_TC_VALUE] :
+                    temperatures[channel] = float(value)
+
+            influx.send(temperatures)
+            time.sleep(1 - (time.time() - starttime) % 1)
+
+    except (HatError, ValueError) as error:
+        print('\n', error)
